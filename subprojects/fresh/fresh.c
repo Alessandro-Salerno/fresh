cbe303d599223ff69ff65239f41807b8500174f4
Alessandro-Salerno <oxygen05@outlook.it>
2025-06-30 13:21:27 +0200

    removed tte, changed fresh startup

--- subprojects/fresh/fresh.c
+++ subprojects/fresh/fresh.c
@@ -18,37 +18,17 @@
 // AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
 
 #include <fresh/parse.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <sysdeps/intf.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include <fresh/custom_crt.h>
-
-struct strtup {
-    const char *s1;
-    const char *s2;
-};
-
-static const char *SHELL_LOGO = "              ooooooooooo\n"
-                                "          ooooooooooooooooooo\n"
-                                "       oooooooo   oooo   oooooooo\n"
-                                "     ooooooo      oooo      ooooooo\n"
-                                "    oooooo        oooo        oooooo\n"
-                                "   oooooo         oooo         oooooo\n"
-                                "   ooooo          oooo          ooooo\n"
-                                "  oooooo         oooooo         oooooo\n"
-                                "  oooooo        oooooooo        oooooo\n"
-                                "   ooooo      oooooooooooo      ooooo\n"
-                                "   oooooo   oooo  oooo  oooo   oooooo\n"
-                                "    oooooooooo    oooo    oooooooooo\n"
-                                "     oooooooo     oooo     oooooooo\n"
-                                "       oooooooo   oooo   oooooooo\n"
-                                "          oooooooooooooooooooo\n"
-                                "              oooooooooooo\n";
 
 int put(int c) {
     char buf[2];
@@ -62,73 +42,34 @@ int put(int c) {
     return EOF;
 }
 
-static void print_logo(void) {
-    printf("\n\n");
-    size_t logo_len = strlen(SHELL_LOGO);
-    size_t line     = 0;
-    size_t line_i   = 0;
+static void print_rainbow(int len) {
+    int rainbow_colors[7] = {196, 208, 226, 46, 21, 93, 201};
+    int segment_len       = len / 7;
+    int remaining         = len % 7; // Extra chars to distribute
 
-    unsigned long colors[]  = {196, 214, 190, 34, 45, 21, 129, 129};
-    size_t        color_idx = 0;
-
-    // hacky thing for testing
-    struct strtup data[] = {
-        {NULL, NULL},
-        {NULL, NULL},
-        {NULL, NULL},
-        {NULL, NULL},
-        {"            \033[0;34mfresh 0.0.1", ""},
-        {"           -----------", ""},
-        {"           \033[0;34mOS\033[0;0m:      ", SYS_OS_NAME},
-        {"          \033[0;34mKernel\033[0;0m:  ", SYS_KERNEL_NAME},
-        {"          \033[0;34mCPU\033[0;0m:     ", SYS_CPU_NAME},
-        {"           \033[0;34mGPU:\033[0;0m:    ", SYS_GPU_NAME},
-        {"           \033[0;34mMemory:\033[0;0m: ", SYS_MEMORY}};
-
-    for (size_t i = 0; i < logo_len; i++) {
-        if (0 == line_i) {
-            unsigned long color = colors[color_idx];
-            printf("\033[38;5;%um", color);
-        }
-
-        if ('\n' != SHELL_LOGO[i]) {
-            put(SHELL_LOGO[i]);
-            line_i++;
-        }
-
-        if ('\n' == SHELL_LOGO[i]) {
-            if (line < 11 && NULL != data[line].s1) {
-                printf("\033[0;0m%s", data[line].s1);
-
-                if (NULL != data[line].s2) {
-                    printf("%s", data[line].s2);
-                } else {
-                    printf("N/A");
-                }
-            }
-
-            line++;
-            if (0 == line % 2) {
-                color_idx++;
-            }
-            line_i = 0;
-            printf("\033[0;0m\n");
+    for (int i = 0; i < 7; i++) {
+        int chars =
+            segment_len + (i < remaining ? 1 : 0); // distribute remainder
+        printf("\033[38;5;%d;48;5;%dm", rainbow_colors[i], rainbow_colors[i]);
+        for (int j = 0; j < chars; j++) {
+            put(' ');
         }
     }
 
-    printf("\n\n");
+    printf("\033[0m\n"); // Reset colors and end line
 }
 
 int main(int argc, char *const argv[]) {
     (void)argc;
     (void)argv;
-    print_logo();
-    printf("Welcome to fresh, the freestanding shell!\n");
-    printf("Copyright (C) 2024 Alessandro Salerno\n\n");
 
-    printf("Credits to Bob Allison for the logo! Taken from "
-           "https://ascii.co.uk/art/peace\n");
-    printf("> fresh stands for freedom (do you get the pun?)\n\n");
+    struct winsize w;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &w) != -1) {
+        print_rainbow(w.ws_col);
+    }
+
+    printf("Welcome to fresh, the freestanding shell!\n");
+    printf("Copyright (C) 2024 - 2025 Alessandro Salerno\n\n");
 
     while (1) {
         char buf[2048];
